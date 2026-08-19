73dcf7dfd08775b2062be783b6b06085541e8d91
flexmaxxing <ricardo.miranda@flex.com>
2026-08-19 12:54:16 -0700

    Update enableLocalBootloader.sh
    adding reboot line and updating prompt

--- enableLocalBootloader.sh
+++ enableLocalBootloader.sh
@@ -2,7 +2,7 @@
 # This will revert back to a local bootloader.
 
 while true; do
-    read -p "Do you wish to revert back to local bootloader? (y/n): " yn
+    read -p "Do you wish to revert back to local bootloader? This will require a reboot. (y/n): " yn
     case $yn in
         [Yy]* ) 
         	echo "Enabling local bootloader"
@@ -10,7 +10,15 @@ while true; do
         	sudo rm /etc/profile.d/90-remote_bootloader.sh    
 			sudo rm /etc/supervisor/waymo_env/remote_bootloader.conf
 
-            echo "Local bootloader has been enabled. Please run 'printenv' to confirm."
+            echo "Local bootloader has been enabled. Please run 'printenv | grep SECURE' to confirm after reboot."
+
+            echo "Now rebooting."
+            for i in {5..1}; do
+                echo -ne "Rebooting in $i seconds... (Press Ctrl+C to abort)\r"
+                sleep 1
+            done
+            echo -e "\nRebooting now!"
+            sudo reboot
             break
             ;;
         [Nn]* ) 
